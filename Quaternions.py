957ffd734274d928f46d2629850ea23a67f9b924
okkokko10 <okko.heinio@gmail.com>
2021-11-19 16:34:51 +0200

    Imported files

---
+++ Quaternions.py
@@ -0,0 +1,142 @@
+from collections import namedtuple
+import math
+'''ii = jj = kk = ijk = -1
+
+
+    i*j = -j*i = k
+    -i*k = k*i = j
+    j*k = -k*j = i
+
+
+    ij = k
+    ik = -j
+    ji = -k
+    jk = i
+    ki = j
+    kj = -i
+    '''
+
+
+class Quaternion:  # (namedtuple('Quaternion', 'r i j k')):
+    r: float
+    i: float
+    j: float
+    k: float
+
+    def __init__(self, r=0, i=0, j=0, k=0):
+        self.r = r
+        self.i = i
+        self.j = j
+        self.k = k
+
+    def __mul__(a, b):
+        if isinstance(b, Quaternion):
+            r = a.r*b.r - a.i*b.i - a.j*b.j - a.k*b.k
+            i = a.j*b.k - a.k*b.j + a.r*b.i + a.i*b.r
+            j = a.k*b.i - a.i*b.k + a.r*b.j + a.j*b.r
+            k = a.i*b.j - a.j*b.i + a.r*b.k + a.k*b.r
+            if isinstance(a, Vector) or isinstance(b, Vector):
+                return Vector.new(r, i, j, k)
+            return Quaternion(r, i, j, k)
+        return a.new(a.r*b, a.i*b, a.j*b, a.k*b)
+
+    def __rmul__(a, b):
+        return a.new(b*a.r, b*a.i, b*a.j, b*a.k)
+
+    def __truediv__(a, b):
+        return a.new(a.r/b, a.i/b, a.j/b, a.k/b)
+
+    def __add__(a, b):
+        return a.new(a.r+b.r, a.i+b.i, a.j+b.j, a.k+b.k)
+
+    def __sub__(a, b):
+        return a.new(a.r-b.r, a.i-b.i, a.j-b.j, a.k-b.k)
+
+    @classmethod
+    def new(cls, r, i, j, k):
+        self = cls.__new__(cls)
+        Quaternion.__init__(self, r, i, j, k)
+        return self
+
+    def normSq(self): return self.r**2+self.i**2+self.j**2+self.k**2
+    def norm(self): return math.sqrt(self.normSq())
+    def conjugate(self): return Quaternion(self.r, -self.i, -self.j, -self.k)
+    def versor(self): return self/self.norm()
+    def reciprocal(self) -> 'Quaternion': return self.conjugate()/self.normSq()
+
+    def GetVector(self): return Vector(self.i, self.j, self.k)
+
+    def Rotate(self, vector: 'Vector') -> 'Vector':
+        'rotates the given vector\n\n(p*q).Rotate(v) == p.Rotate(q.Rotate(v))\n\nalso called conjugation'
+        return self * vector * self.reciprocal()
+
+    def RotateInverse(self, vector: 'Vector') -> 'Vector':
+        'rotates the given vector in the opposite direction\n\np.RotateInverse(p.Rotate(v)) == v'
+        return self.reciprocal() * vector * self
+
+    def __str__(self): return '{}({} {} {} {})'.format(self.__class__.__name__, self.r, self.i, self.j, self.k)
+
+
+class Vector(Quaternion):
+    # def __new__(cls,x, y, z): return Quaternion.__new__(cls,0, x, y, z)
+
+    def __init__(self, i=0, j=0, k=0):
+        super().__init__(0, i, j, k)
+        # self.r = 0
+        # self.i = i
+        # self.j = j
+        # self.k = k
+
+    def RotationAround(self: 'Vector', angle):
+        s = math.sin(angle/2)
+        return Quaternion(math.cos(angle/2), self.i*s, self.j*s, self.k*s)
+
+    def RotatedAroundAxis(self: 'Vector', axis: 'Vector', angle) -> 'Vector':
+        rotator = axis.RotationAround(angle)
+        return rotator.Rotate(self)
+
+
+class Transform:
+    def __init__(self, position: 'Vector', rotation: 'Quaternion'):
+        self.position = position
+        self.rotation = rotation
+
+    def Localize(self, other: 'Transform'):
+        'if self and other are in w space, what is other in self space'
+        return Transform(self.LocalizePosition(other.position),
+                         self.LocalizeRotation(other.rotation))
+
+    def LocalizePosition(self, other):
+        return self.rotation.RotateInverse(other - self.position)
+
+    def LocalizeRotation(self, other: 'Quaternion'):
+        return self.rotation.reciprocal() * other
+
+    def LocalizeDirection(self, other: 'Vector'):
+        return self.rotation.RotateInverse(other)
+
+    def Globalize(self, other: 'Transform or Vector or Quaternion'):
+        'if self is in w space and other is in self space, what is other in w space'
+        return Transform(self.GlobalizePosition(other.position),
+                         self.GlobalizeRotation(other.rotation))
+
+    def GlobalizePosition(self, other: 'Vector'):
+        return self.position + self.rotation.Rotate(other)
+
+    def GlobalizeRotation(self, other: 'Quaternion'):
+        return self.rotation * other
+
+    def GlobalizeDirection(self, other: 'Vector'):
+        return self.rotation.Rotate(other)
+
+
+if __name__ == '__main__':
+    A = Quaternion(1, 2, 3, 4)
+    print(A)
+    B = Quaternion(1, 2, 3, 4)
+    print('B', B)
+
+    C = A*B
+    print(C)
+    D = A.reciprocal()*C
+    print(D)
---
+++ Show.py
@@ -0,0 +1,247 @@
+import random
+import pygame
+
+from Quaternions import *
+
+
+class ObjectStorage:
+    'Empty object for storing objects'
+    pass
+
+
+class Updater:
+    "use Updater().Setup() to quickly initialize an updater"
+    canvas: 'Canvas' = None
+    inputs: 'Inputs' = None
+    func = None
+    framerate = None
+    objects: 'ObjectStorage' = None
+
+    _clock: 'pygame.time.Clock' = None
+    _running: 'bool' = None
+    deltaTime: 'int' = None
+
+    def Setup(self, func=None, canvas=None, inputs=None, framerate=None, objectStorage=None):
+        "func is a function that takes the updater as argument and is called every frame after updater.Play() is called"
+        if canvas or not self.canvas:
+            self.InitDisplayCanvas(canvas)
+        if inputs or not self.inputs:
+            self.InitInputs(inputs)
+        if func:
+            self.SetFunc(func)
+        if framerate or not self.framerate:
+            DEFAULT_FRAMERATE = 40
+            self.SetFramerate(framerate or DEFAULT_FRAMERATE)
+        if objectStorage or not self.objects:
+            self.InitObjectStorage(objectStorage)
+        return self
+
+    def Play(self):
+        'func(self)'
+        self._clock = pygame.time.Clock()
+        self._running = True
+        while self._running:
+            self.deltaTime = self._clock.tick(self.framerate)
+            if pygame.event.get(pygame.QUIT):
+                self.Stop()
+            self.events = pygame.event.get()
+            self._Update_inputs()
+            self.func(self)
+
+            pygame.display.update()
+        return self
+
+    def _Update_inputs(self):
+        self.inputs.UpdateInputs(self.events, self.deltaTime)
+
+    def InitDisplayCanvas(self, canvas: 'Canvas' = None):
+        self.canvas = canvas or Canvas(pygame.display.set_mode((800, 800)))
+        return self.canvas
+
+    def InitInputs(self, inputs: 'Inputs' = None):
+        self.inputs = inputs or Inputs()
+        return self.inputs
+
+    def SetFunc(self, func):
+        self.func = func
+
+    def SetFramerate(self, framerate):
+        self.framerate = framerate
+
+    def InitObjectStorage(self, objectStorage: 'ObjectStorage' = None):
+        self.objects = objectStorage or ObjectStorage()
+
+    def get_canvas(self):
+        return self.canvas
+
+    def get_framerate(self):
+        return self.framerate
+
+    def get_inputs(self):
+        return self.inputs
+
+    def get_objects(self):
+        return self.objects
+
+    def get_deltaTime(self):
+        return self.deltaTime
+
+    def Stop(self):
+        self._running = False
+        return
+
+
+class Canvas:
+    def __init__(self, surface: pygame.surface.Surface):
+        self.surface = surface
+        self.height = surface.get_height()
+        self.width = surface.get_width()
+
+    def Line(self, pos1, pos2, width, color):
+        pygame.draw.line(self.surface, color, self.convert(
+            pos1), self.convert(pos2), width)
+
+    def Lines(self, poslist, width, color):
+        closed = False
+        pygame.draw.lines(self.surface, color, closed, [
+                          self.convert(pos) for pos in poslist], width)
+
+    def Circle(self, pos, radius, color):
+        pygame.draw.circle(self.surface, color, self.convert(pos), radius)
+
+    def convert(self, pos):
+        return (int(pos[0]*self.width)+self.width//2, -int(pos[1]*self.height)+self.height//2)
+
+    def Fill(self, color):
+        self.surface.fill(color)
+
+    def LockSurface(self):
+        self.surface.lock()
+
+    def UnlockSurface(self):
+        self.surface.unlock()
+
+
+class Camera:
+    def __init__(self):
+        self.transform = Transform(Vector(0, 0, 0), Quaternion(1))
+        self.height = 1
+        self.width = 1
+
+    def ProjectPosition(self, other: 'Vector'):
+        p = self.transform.LocalizePosition(other)
+        if p.k <= 0:
+            return None
+        if abs(p.i)*2 > self.width*p.k or abs(p.j)*2 > self.height*p.k:
+            return None
+        x, y = p.i/p.k, p.j/p.k
+        return x, y
+
+    def DrawLines(self, canvas: 'Canvas', vectors, width, color):
+        poslist = [p for p in (self.ProjectPosition(v) for v in vectors) if p]
+        if len(poslist) >= 2:
+            canvas.Lines(poslist, width, color)
+
+    def DrawDots(self, canvas: 'Canvas', vectors, radius, color):
+        for v in vectors:
+            p = self.ProjectPosition(v)
+            if p:
+                canvas.Circle(p, radius, color)
+            pass
+
+
+class Inputs:
+    def __init__(self):
+        self.ups = set()
+        self.downs = set()
+        self.helds = {}
+        pass
+
+    def _setDown(self, key):
+        self.downs.add(key)
+        self.helds[key] = 0
+        return
+
+    def _setUp(self, key):
+        self.ups.add(key)
+        return
+
+    def UpdateInputs(self, events, deltaTime):
+        for k in self.ups:
+            del self.helds[k]
+        for k in self.helds:
+            self.helds[k] += deltaTime
+        self.ups.clear()
+        self.downs.clear()
+        for e in events:
+            if e.type == pygame.KEYDOWN:
+                self._setDown(e.key)
+            elif e.type == pygame.KEYUP:
+                self._setUp(e.key)
+
+    def IsHeld(self, key):
+        return key in self.helds
+
+    def IsUp(self, key):
+        return key in self.ups
+
+    def IsDown(self, key):
+        return key in self.downs
+
+    def LockMouse(self):
+        pygame.mouse.set_visible(False)
+        pygame.event.set_grab(True)
+
+    def UnlockMouse(self):
+        pygame.mouse.set_visible(True)
+        pygame.event.set_grab(False)
+
+
+if __name__ == '__main__':
+
+    C = Camera()
+    C.transform.position = Vector(0, 0, 0)
+
+    # den = 5
+    # points = [Vector(i, j, k)/den for i in range(3*den) for j in range(3*den) for k in range(3*den)]
+    # points += [Vector(i/den, 0, k/den) for i in range(-3*den, 6*den) for k in range(-3*den, 6*den)]
+
+    points = [Vector(random.random()*2-1,  random.random()*2-1, random.random()*0.1+2) for _ in range(500)]
+
+    mx, my = 0, 0
+
+    def update(updater: 'Updater', events, deltaTime):
+        canvas = updater.get_canvas()
+        inputs = updater.get_inputs()
+        canvas.Fill((0, 0, 100))
+        canvas.LockSurface()
+        # C.DrawLines(canvas, points, 5, (100, 0, 100))
+        C.DrawDots(canvas, points, 5, (100, 0, 000))
+        canvas.UnlockSurface()
+        for e in events:
+            print(e)
+        global mx, my
+        mdx, mdy = pygame.mouse.get_rel()
+        mx += mdx
+        my += mdy
+        # C.transform.rotation *= Vector(1,0,0).RotationAround(-mdy/400)*(Vector(0,1,0).RotationAround(-mdx/400))
+        rotationX = Vector(0, 1, 0).RotationAround(mx/200)
+        rotationY = Vector(1, 0, 0).RotationAround(my/200)
+        WASDvector = Vector(inputs.IsHeld(pygame.K_d)-inputs.IsHeld(pygame.K_a), 0,
+                            inputs.IsHeld(pygame.K_w)-inputs.IsHeld(pygame.K_s))
+
+        C.transform.rotation = rotationX * rotationY
+        C.transform.position += rotationX.Rotate(WASDvector*deltaTime*0.001)
+
+        if inputs.IsDown(pygame.K_t):
+            inputs.LockMouse()
+        if inputs.IsDown(pygame.K_y):
+            inputs.UnlockMouse()
+
+        return
+
+    Upd = Updater().Setup(update, 40)
+
+    Upd.get_inputs().LockMouse()
+
+    Upd.Play()
