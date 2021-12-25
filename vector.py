9551f4be9677e6f50bd34ee034aaa47be8af6625
okkokko10 <okko.heinio@gmail.com>
2021-12-25 19:54:44 +0200

    Update vector.py

--- vector.py
+++ vector.py
@@ -32,7 +32,7 @@ class Vector:
         return (0, 1)
 
     def __add__(self, other):
-        return Vector(*(self[k].__add__(other[k]) for k in self.keys()))
+        return Vector(*(a.__add__(b) for a, b in zip(self, other)))
 
     # def __sub__(self, other):
     #     return Vector(
@@ -41,61 +41,64 @@ class Vector:
     #     )
 
     def __sub__(self, other):
-        return Vector(*(self[k].__sub__(other[k]) for k in self.keys()))
+        return Vector(*(a.__sub__(b) for a, b in zip(self, other)))
 
     def __mul__(self, other):
         if isinstance(other, Vector):
-            return Vector(*(self[k].__mul__(other[k]) for k in self.keys()))
+            return Vector(*(a.__mul__(b) for a, b in zip(self, other)))
         else:
-            return Vector(*(self[k].__mul__(other) for k in self.keys()))
+            return Vector(*(a.__mul__(other) for a in self))
 
     def __rmul__(self, other):
         if isinstance(other, Vector):
-            return Vector(*(self[k].__rmul__(other[k]) for k in self.keys()))
+            return Vector(*(a.__rmul__(b) for a, b in zip(self, other)))
         else:
-            return Vector(*(self[k].__rmul__(other) for k in self.keys()))
+            return Vector(*(a.__rmul__(other) for a in self))
 
     def __truediv__(self, other):
-        return Vector(*(self[k].__truediv__(other) for k in self.keys()))
+        return Vector(*(a.__truediv__(other) for a in self))
 
     def __le__(self, other):
-        return Vector(*(self[k].__le__(other[k]) for k in self.keys()))
+        return Vector(*(a.__le__(b) for a, b in zip(self, other)))
 
     def __lt__(self, other):
-        return Vector(*(self[k].__lt__(other[k]) for k in self.keys()))
+        return Vector(*(a.__lt__(b) for a, b in zip(self, other)))
 
     def __ge__(self, other):
-        return Vector(*(self[k].__ge__(other[k]) for k in self.keys()))
+        return Vector(*(a.__ge__(b) for a, b in zip(self, other)))
 
     def __gt__(self, other):
-        return Vector(*(self[k].__gt__(other[k]) for k in self.keys()))
+        return Vector(*(a.__gt__(b) for a, b in zip(self, other)))
 
     def __neg__(self):
-        return Vector(*(self[k].__neg__() for k in self.keys()))
+        return Vector(*(a.__neg__() for a in self))
 
     def __pos__(self):
-        return Vector(*(self[k].__pos__() for k in self.keys()))
+        return Vector(*(a.__pos__() for a in self))
 
     def __floor__(self):
-        return Vector(*(self[k].__floor__() for k in self.keys()))
+        return Vector(*(a.__floor__() for a in self))
 
     def __int__(self):
-        return Vector(*(self[k].__int__() for k in self.keys()))
+        return Vector(*(a.__int__() for a in self))
 
     def __str__(self):
-        return 'V({})'.format(' '.join(self[k].__str__() for k in self.keys()))
+        return 'V({})'.format(' '.join(a.__str__() for a in self))
 
-    def lengthSq(self):
-        return sum(self[k]**2 for k in self.keys())
+    def lengthSq(self) -> float:
+        return sum(k**2 for k in self)
 
-    def length(self): return math.sqrt(self.lengthSq())
+    def length(self) -> float: return math.sqrt(self.lengthSq())
     def dotProduct(self, other: 'Vector') -> float: return sum(self * other)
 
-    def project(self, other: 'Vector'):
+    def project(self, other: 'Vector') -> 'Vector':
         return self.dotProduct(other) / self.lengthSq() * self
 
-    def projectScalar(self, other: 'Vector'):
+    def projectScalar(self, other: 'Vector') -> float:
         return self.dotProduct(other) / self.length()
+
+    def projectScalarSq(self, other: 'Vector') -> float:
+        return self.dotProduct(other)**2 / self.lengthSq()
 
     def complexMul(self, other: 'Vector'):
         return Vector(self.x * other.x - self.y * other.y, self.x * other.y + self.y * other.x)
@@ -116,3 +119,7 @@ class Vector:
     def round(self, size: 'Vector') -> 'Vector':
 
         return Vector(*((self[k] // size[k] * size[k]) for k in self.keys()))
+
+    def normal(self):
+        "a vector perpendicular to this one"
+        return Vector(-self.y, self.x)
