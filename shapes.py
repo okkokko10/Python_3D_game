f2540995fe3ed12641e2c3a780c6ebefc45e332a
okkokko10 <okko.heinio@gmail.com>
2022-02-09 15:07:47 +0200

    did something with MoveAttributes

--- shapes.py
+++ shapes.py
@@ -8,6 +8,18 @@ class Shape:
     pass
 
 
+class Drawable:
+    def Draw(self, canvas: 'Canvas', color=(255, 255, 255), *args, **kwargs):
+        # raise NotImplementedError
+        pass
+
+    def Glow(self, canvas: 'Canvas', color=(100, 255, 100), *args, **kwargs):
+        # raise NotImplementedError
+        pass
+
+    pass
+
+
 class Reflectable:
     def Hit(self, laser: 'Laser') -> 'list[Vector]':
         "does the laser hit, and where"
@@ -18,7 +30,101 @@ class Reflectable:
         raise NotImplementedError
 
 
-class Line(Shape, Reflectable):
+class MoveAttribute:
+    def __init__(self, *args, is_special=False):
+        self.args = args
+        self.is_special = is_special
+        "if the attribute should compare true with only itself"
+
+    def __eq__(self, other):
+        return self is other if self.is_special else (isinstance(other, MoveAttribute) and self.args == other.args)
+
+    def __hash__(self):
+        return hash(self.args)
+    WHOLE: "MoveAttribute"
+
+
+MoveAttribute.WHOLE = MoveAttribute(is_special=True)
+"sentinel for movable that means all attributes"
+
+
+class Movable:
+
+    def movable_attributes(self) -> Iterable[MoveAttribute]:
+        """Override this.
+        must be an iterable of MoveAttribute objects, containing all attributes that are valid inputs to movable_Get and movable_Set
+        attributes must be absolute positions that can be set independently of each other"""
+        return ()
+
+    def movable_Get(self, attr: MoveAttribute) -> Vector:
+        "Override this."
+        return getattr(self, attr)
+
+    def movable_Set(self, attr: MoveAttribute, value: Vector):
+        "Override this."
+        setattr(self, attr, value)
+
+    def movable_Warp(self, attrs: Iterable[MoveAttribute], func: Callable[[Vector], Vector]):
+        "applies func to all given attrs"
+        for attr in attrs:
+            self.movable_Set(attr, func(self.movable_Get(attr)))
+            pass
+        pass
+
+    def movable_closest(self, position: Vector) -> tuple[MoveAttribute, float]:
+        "Maybe override this. Returns closest attribute, and the squared distance to the attribute"
+        def key(k):
+            return (self.movable_Get(k) - position).lengthSq()
+        a = min(self.movable_attributes(), key=key)
+        return a, key(a)
+
+    def movable_distanceSq(self, position: Vector) -> float:
+        "Override this. Return the square distance to the position, for finding the closest movable"
+        return self.movable_closest(position)[0]
+        # raise NotImplementedError
+
+    def movable_OnPick(self, mover: 'Mover', attr: MoveAttribute):
+        "What happens when a mover picks up this Movable"
+        pass
+
+    def MovePartTo(self, attr: MoveAttribute, position: 'Vector'):
+        self.movable_Set(attr, position)
+
+    def Move(self, attr: MoveAttribute, movement: 'Vector'):
+        if attr == MoveAttribute.WHOLE:
+            self.MoveWhole(movement)
+        else:
+            self.movable_Set(attr, self.movable_Get(attr) + movement)
+
+    def MoveWhole(self, movement: 'Vector'):
+        self.Warp(lambda v: v + movement)
+        # for k in self.movable_attributes():
+        #     self.movable_Set(k, self.movable_Get(k) + movement)
+
+    def MoveWholeTo(self, attr: MoveAttribute, position: 'Vector'):
+        "offset the Movable so that the given attribute goes in the given position"
+        self.MoveWhole(position - self.movable_Get(attr))
+
+    def Rotozoom(self, center: Vector, zoom: Vector):
+        "complex multiplies the Movable by the zoom vector, using center as the origin"
+        # might be lossy
+        self.Warp(lambda old_pos: (old_pos - center).complexMul(zoom) + center)
+
+    def Rotate(self, center: Vector, start: Vector, stop: Vector):
+        "rotates the Movable around the center so that what was at start goes to stop"
+        a = start - center
+        b = stop - center
+        self.Rotozoom(center, b.complexDiv(a))
+
+    def Warp(self, func: Callable[[Vector], Vector]):
+        "applies func to attributes' positions"
+        # for k in self.movable_attributes():
+        #     old_pos = self.movable_Get(k)
+        #     self.movable_Set(k, func(old_pos))
+        self.movable_Warp(self.movable_attributes(), func)
+
+
+class Line(Shape, Reflectable, Drawable, Movable):
     def __init__(self, start: Vector, end: Vector, is_cap_a=True, is_cap_b=False):
         self.a = start
         self.b = end
@@ -70,6 +176,15 @@ class Line(Shape, Reflectable):
         if self.is_cap_b:
             canvas.Circle(self.b, width * 3, color)
 
+    def Glow(self, canvas: 'Canvas', color=(100, 255, 100), width=1):
+        offset = width * self.vector.unit().normal()
+        for i in 1, -1:
+            canvas.Line(self.a + offset * i, self.b + offset * i, width, color)
+        # if self.is_cap_a:
+        #     canvas.Circle(self.a, width * 3, color)
+        # if self.is_cap_b:
+        #     canvas.Circle(self.b, width * 3, color)
+
     def Hit(self, laser: 'Laser'):
         a = self.Intersection(laser.line)
         if a and self.isInside(a):
@@ -115,8 +230,14 @@ class Line(Shape, Reflectable):
         else:
             return (self.a - point).lengthSq()
 
+    def movable_attributes(self):
+        return 'a', 'b'
 
-class Sphere(Shape, Reflectable):
+    def movable_distanceSq(self, position: Vector) -> float:
+        return self.distanceSegmentSq()
+
+
+class Sphere(Shape, Reflectable, Drawable, Movable):
 
     def __init__(self, center: Vector, radiusSq: float):
         self.center = center
@@ -154,121 +275,63 @@ class Sphere(Shape, Reflectable):
     def Draw(self, canvas: 'Canvas', color=(255, 255, 255), width=1):
         canvas.Circle(self.center, self.radius, color, width)
 
+    def Glow(self, canvas: 'Canvas', color=(100, 255, 100), width=1):
+        canvas.Circle(self.center, self.radius - width, color, width)
+        canvas.Circle(self.center, self.radius + width, color, width)
 
-class Movable:
-    def movable_attributes(self) -> tuple[str]:
-        """Override this. Empty string is a keyword for whole.
-        attributes must be absolute positions that can be set independently of each other"""
-        return ()
-
-    def movable_closest(self, position: Vector) -> tuple[str, float]:
-        "Maybe override this. Returns closest attribute, and the squared distance to the attribute"
-        def key(k):
-            return (self.movable_Get(k) - position).lengthSq()
-        a = min(self.movable_attributes(), key=key)
-        return a, key(a)
-
-    def movable_distanceSq(self, position: Vector) -> float:
-        "Override this. Return the square distance to the position, for finding the closest movable"
-        return self.movable_closest(position)[0]
-        # raise NotImplementedError
-
-    def movable_OnPick(self, mover: 'Mover', attr: 'str'):
-        pass
-
-    def movable_Get(self, attr: str) -> Vector:
-        return getattr(self, attr)
-
-    def movable_Set(self, attr: str, value: Vector):
-        assert attr
-        setattr(self, attr, value)
-
-    def MovePartTo(self, attr: str, position: 'Vector'):
-        self.movable_Set(attr, position)
-
-    def Move(self, attr: str, movement: 'Vector'):
-        if attr:  # !=""
-            self.movable_Set(attr, self.movable_Get(attr) + movement)
-        else:
-            self.MoveWhole(movement)
-
-    def MoveWhole(self, movement: 'Vector'):
-        for k in self.movable_attributes():
-            self.movable_Set(k, self.movable_Get(k) + movement)
-
-    def MoveWholeTo(self, attr: str, position: 'Vector'):
-        self.MoveWhole(position - self.movable_Get(attr))
-
-    def Rotozoom(self, center: Vector, zoom: Vector):
-        # might be lossy
-        self.Warp(lambda old_pos: (old_pos - center).complexMul(zoom) + center)
-
-    def Rotate(self, center: Vector, start: Vector, stop: Vector):
-        a = start - center
-        b = stop - center
-        self.Rotozoom(center, b.complexDiv(a))
-
-    def Warp(self, func: Callable[[Vector], Vector]):
-        "applies func to attributes"
-        for k in self.movable_attributes():
-            old_pos = self.movable_Get(k)
-            self.movable_Set(k, func(old_pos))
-
-
-class MovableLine(Line, Movable):
     def movable_attributes(self):
-        return 'a', 'b'
-
-    def movable_distanceSq(self, position: Vector) -> float:
-        return self.distanceSegmentSq()
-
-
-class MovableSphere(Sphere, Movable):
-    def movable_attributes(self) -> tuple[str]:
         return "center", "edge"
 
     def movable_distanceSq(self, position: Vector) -> float:
         return self.movable_closest(position)[1]
+
 
 # TODO: rotation, highlighting an object
 
 
 class Mover:
     def __init__(self):
-        self._position = Vector(0, 0)
-        self._movement = Vector(0, 0)
-        self._picked = None
-        self._pickedAttr = None
-        self._down = False
+        "all attributes are read-only"
+        self.position = Vector(0, 0)
+        "the position the Mover is at"
+        self.movement = Vector(0, 0)
+        "the change in self.position from last frame"
+        self.picked = None
+        "the Movable that is being moved"
+        self.picked_attr = None
+        "the attribute of self.picked that is being moved"
+        self.down = False
         "if on the previous frame the mover was pressed down"
-        self._whole = False
+        self.whole = False
         "if the whole Movable is moved"
         pass
 
     def update(self, position: Vector, down: bool, movables: list[Movable], whole=False) -> None:
-        self._movement = position - self._position
-        self._position = position
-        self._whole = whole
-        if self._picked and self._picked not in movables:
-            self._picked, self._pickedAttr = None, None
-        if down and not self._down:  # down
-            self._picked, self._pickedAttr = self.findClosest(self._position, movables)
-            if self._picked and self._pickedAttr:  # move the picked point to the cursor, with the other points if total is True
-                self._picked.movable_OnPick(self, self._pickedAttr)
-                if self._whole:
-                    self._picked.MoveWholeTo(self._pickedAttr, self._position)
+        self.movement = position - self.position
+        self.position = position
+        self.whole = whole
+        if self.picked and self.picked not in movables:
+            self.picked, self.picked_attr = None, None
+        if down and not self.down:  # down
+            self.picked, self.picked_attr = self.findClosest(self.position, movables)
+            if self.picked is not None:  # and self.picked_attr:  # if above findClosest returned something else than (None,None)
+                # move the picked point to the cursor, with the other points if total is True
+                self.picked.movable_OnPick(self, self.picked_attr)
+                if self.whole:
+                    self.picked.MoveWholeTo(self.picked_attr, self.position)
                 else:
-                    self._picked.MovePartTo(self._pickedAttr, self._position)
-        elif self._down and not down:  # up
-            self._picked, self._pickedAttr = None, None
-        elif self._picked and self._down:  # do not execute immediately after picking
-            if self._whole:
-                self._picked.MoveWhole(self._movement)
+                    self.picked.MovePartTo(self.picked_attr, self.position)
+        elif self.down and not down:  # up
+            self.picked, self.picked_attr = None, None
+        elif self.picked is not None and self.down:  # do not execute immediately after picking
+            if self.whole:
+                self.picked.MoveWhole(self.movement)
             else:
-                self._picked.Move(self._pickedAttr, self._movement)
-        self._down = down
+                self.picked.Move(self.picked_attr, self.movement)
+        self.down = down
 
-    def findClosest(self, position: Vector, movables: list[Movable], maxDistanceSq=-1) -> tuple[Movable, str] | tuple[None, None]:
+    def findClosest(self, position: Vector, movables: list[Movable], maxDistanceSq=-1) -> tuple[Movable, MoveAttribute] | tuple[None, None]:
+        "returns (None,None) if nothing is found"
         closestP = None
         closestAttr = None
         distanceSq = maxDistanceSq
@@ -372,13 +435,13 @@ def main():
             mpos = Vector(*inputs.get_mouse_position()).round(GRID) + GRID / 2
             if inputs.keyDown(pygame.K_UP) or inputs.mouseDown(1):
                 if self.one:
-                    self.shapes.append(MovableLine(self.one, mpos))
+                    self.shapes.append(Line(self.one, mpos))
                     self.one = None
                 else:
                     self.one = mpos
             if inputs.keyPressed(pygame.K_LEFT) or inputs.mousePressed(6):
                 if self.one:
-                    self.shapes.append(MovableSphere(self.one, (self.one - mpos).lengthSq()))
+                    self.shapes.append(Sphere(self.one, (self.one - mpos).lengthSq()))
                     self.one = None
             movedown = inputs.keyPressed(pygame.K_DOWN) or inputs.mousePressed(3)
             movewhole = inputs.keyPressed(pygame.K_RSHIFT) or inputs.keyPressed(pygame.K_LSHIFT)
@@ -398,6 +461,8 @@ def main():
                     canvas.Circle(a.line.a, 1 + min(b, 100) // 10, (0, 0, min(b, 100)))
             for line in self.shapes:
                 line.Draw(canvas)
+            if self.mover.picked:
+                self.mover.picked.Glow(canvas)
             s = pygame.Surface((GRID.x, GRID.y,))
             s.fill((50, 0, 50))
             canvas.surface.blit(s, canvas.convert(mpos - GRID / 2), special_flags=pygame.BLEND_ADD)
